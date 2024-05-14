d4e4a7446cdb37b728d913ba1b1361878e4b2f73
Ampare <visarut.thk@gmail.com>
2024-05-14 13:03:08 +0700

    Assignment:JS-Functions

--- ex-2.js
+++ ex-2.js
@@ -1,1 +1,21 @@
 // Start coding here
+let add = function (a, b) {
+  return a + b;
+};
+
+let subtract = function (a, b) {
+  return a - b;
+};
+
+let multipy = function (a, b) {
+  return a * b;
+};
+
+let divide = function (a, b) {
+  return a / b;
+};
+
+console.log(add(10, 5));
+console.log(subtract(10, 5));
+console.log(multipy(10, 5));
+console.log(divide(10, 5));
