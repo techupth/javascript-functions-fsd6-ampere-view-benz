0b4c0a552521026ce1ce22d4a8fd9f107908b6db
revieww12 <review120244@gmail.com>
2024-05-14 13:55:15 +0700

    feat: add code

--- ex-3.js
+++ ex-3.js
@@ -1,1 +1,21 @@
 // Start coding here
+
+//add number
+let add = (a, b) => a + b;
+const answer1 = add(10, 5);
+console.log(answer1);
+
+//subtract number
+let subtract = (a, b) => a - b;
+const answer2 = subtract(10, 5);
+console.log(answer2);
+
+//multiply number
+let multiply = (a, b) => a * b;
+const answer3 = multiply(10, 5);
+console.log(answer3);
+
+//divide number
+let divide = (a, b) => a / b;
+const answer4 = divide(10, 5);
+console.log(answer4);
